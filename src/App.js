19d8c58c7e815df863870d609c4d6bff2cd0152e
rafael Pietri-Davila <rafael.pietri@gmail.com>
2022-01-12 13:59:03 +0100

    added checkout page, check out item component to use selectors from reselct

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component copy';
 import { connect } from 'react-redux';
@@ -9,6 +10,8 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import './App.css';
 
 import { setCurrentUser }from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selector';
+import { createStructuredSelector } from 'reselect';
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -47,15 +50,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+          <Route path='/checkout' component={CheckoutPage} />
+          <Route 
+            exact 
+            path='/signin' 
+            render={()=> 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+              <SignInAndSignUpPage />
+              )} 
+          />
         </Switch>
       </div>
     );
   }    
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,9 +2,14 @@ import  { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
-export const selectCartItems = createSelector(
+export const    selectCartItems = createSelector(
     [selectCart],
-    (cart) => cart.cartItems
+    cart => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
@@ -14,3 +19,13 @@ export const selectCartItemsCount = createSelector(
         0
     )
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) => 
+            accumulatedQuantity + cartItem.quantity * cartItem.price, 
+        0
+    )
+
+)
